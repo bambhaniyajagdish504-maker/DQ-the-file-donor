9d9f4b203a89eeb1b5d6b99e397495f214ca3813
bambhaniyajagdish504-maker <bambhaniyajagdish504@gmail.com>
2025-11-30 06:05:00 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot running OK!"
+
+def start_web_server():
+    threading.Thread(target=lambda: app.run(host="0.0.0.0", port=8000)).start()
