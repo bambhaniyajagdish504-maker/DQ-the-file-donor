5ebef392329522141ce7a0d66be3c564c9041286
bambhaniyajagdish504-maker <bambhaniyajagdish504@gmail.com>
2025-11-30 05:57:27 +0530

    Update aap.py

--- aap.py
+++ aap.py
@@ -1,20 +1,11 @@
-from aiohttp import web
-import asyncio
+from flask import Flask
+import threading
 
-async def health(request):
-    return web.Response(text="OK")
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running fine!"
 
 def start_web_server():
-    app = web.Application()
-    app.router.add_get("/", health)
-    app.router.add_get("/health", health)
-
-    runner = web.AppRunner(app)
-
-    async def start():
-        await runner.setup()
-        site = web.TCPSite(runner, "0.0.0.0", 8000)
-        await site.start()
-
-    loop = asyncio.get_event_loop()
-    loop.create_task(start())
+    threading.Thread(target=lambda: app.run(host="0.0.0.0", port=8000)).start()
